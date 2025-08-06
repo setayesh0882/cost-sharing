ee6a624b7703e6e478679e54eb7bdda97bf061c9
unknown <setayesh13822000@gmail.com>
2025-08-06 12:10:34 +0330

    create todolist by react

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,38 @@
-import { useState } from "react";
-import axios from "axios";
+import { useState } from 'react';
 
 function App() {
-  
-  const [photo, setPhoto] = useState({ id: "", title: "", url: "" });
+  const [task, setTask] = useState('');
+  const [tasks, setTasks] = useState([]);
 
-  const fetchData = () => {
-    axios.get("https://jsonplaceholder.typicode.com/photos/1")
-      .then((res) => {
-        setPhoto(res.data);
-      })
+  const addTask = () => {
+    setTasks([...tasks, task]);
+    setTask('');
+  };
+
+  const deleteTask = (taskToDelete) => {
+    const newTasks = tasks.filter(t => t !== taskToDelete);
+    setTasks(newTasks);
   };
 
   return (
     <div>
-      <button onClick={fetchData}>fetch data</button>
+      <h2>لیست فعالیت‌ها</h2>
 
-      <p>{photo.id}</p>
-      <p>{photo.title}</p>
-      <p>{photo.url}</p>
+      <input
+        type="text"
+        value={task}
+        onChange={(e) => setTask(e.target.value)}
+        placeholder="یک فعالیت جدید وارد کنید"/>
+      <button onClick={addTask}>افزودن</button>
+
+      <ul>
+        {tasks.map((task, index) => (
+          <li key={index}>
+            {task}
+            <button onClick={() => deleteTask(task)}>حذف</button>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
